088f3263ae750196ffea5d6c20aa0d1536c4ec64
GYPZY_TECH <99523108+GYPZYTECHS@users.noreply.github.com>
2022-06-17 05:33:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/GYPZYTECHS/SAMANTHA_V8_PRO
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TovinoBotV3
+  git clone $UPSTREAM_REPO /SAMANTHA_V8_PRO
 fi
 cd /SAMANTHA_V8_PRO
 pip3 install -U -r requirements.txt
