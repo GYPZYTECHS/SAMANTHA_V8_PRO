9c4af3d13f5b634fca00c6de6cff8500069de015
GYPZY_TECH <99523108+GYPZYTECHS@users.noreply.github.com>
2022-06-17 09:56:48 -0700

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -150,15 +150,14 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.answer('✷Requested movie':{query}
+            k = await query.message.edit('✷Requested movie':{query}
+"""Not available Right Now
+✷Possible Causes : 🤔'
 
-'Not available Right Now'
-'✷Possible Causes : 🤔'
-
-'۝Not released yet
-'۝Unwanted texts in Msgs
-'۝Asking theatre prints
-'۝Not in my Database')
+۝Not released yet
+۝Unwanted texts in Msgs
+۝Asking theatre prints
+۝Not in my Database"""),
                                         
             await asyncio.sleep(10)
             await k.delete()
