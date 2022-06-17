40067362dff9f3d65c82b8ede90b431a6a25f86e
GYPZY_TECH <99523108+GYPZYTECHS@users.noreply.github.com>
2022-06-17 08:52:34 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GYPZYTECHS/SAMANTHA_V8_PRO
+  git clone https://github.com/DKBOTx/TovinoBotV3.git /TovinoBotV3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /SAMANTHA_V8_PRO
+  git clone $UPSTREAM_REPO /TovinoBotV3
 fi
-cd /SAMANTHA_V8_PRO
+cd /TovinoBotV3
 pip3 install -U -r requirements.txt
-echo "Starting SAMANTHA...."
+echo "Starting 𝘋𝘒_𝘉𝘖𝘛𝘹...."
 python3 bot.py
